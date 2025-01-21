1de236bf084191eccb9fe1a0676d04e9debe206f
Ksenia Klepikova <104532642+kseenik@users.noreply.github.com>
2025-01-22 00:09:07 +0300

    Update model.py

--- model.py
+++ model.py
@@ -13,7 +13,6 @@ import nltk
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, accuracy_score
 from imblearn.over_sampling import SMOTE
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
@@ -66,7 +65,6 @@ X_train, X_test, y_train, y_test = train_test_split(X_resampled, y_resampled, te
 # Обучение модели
 model = RandomForestClassifier(random_state=42)
 model.fit(X_train, y_train)
-
 
 # Предсказания модели
 y_pred = model.predict(X_test)
